7fb2dc2e66c7d548f0f8e14bc858e4598a9f7321
fernando-daw <flagonovoa@danielcastelao.org>
2023-02-15 16:16:02 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import com.castelao.EntradaSalida;
+
+public class Main {
+    public static void main(String[] args) {
+        EntradaSalida.salida("bien!!",EntradaSalida.SALIDA_WINDOW);
+    }
+}
